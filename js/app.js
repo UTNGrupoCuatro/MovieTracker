3d2769e4993a713b007c0b4053887dcdd265b2db
Martín Masignano <martinmasignano@gmail.com>
2017-11-20 17:17:42 -0300

    Agrego owl carousel, leaflet map: carouseles y mapa en la home

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  }
-})
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      margin: 10,
+      items: 3,
+      stagePadding: 15,
+      loop: false,
+      nav: false,
+      dots: false
+  });
+});
+
+// mapa
+var map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
